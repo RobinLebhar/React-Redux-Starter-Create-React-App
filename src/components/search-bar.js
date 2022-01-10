37623f83b4eaf233276d161dc3314e7ffcd08f24
MatisGalvin <matis.galvin@epsi.fr>
2022-01-10 17:05:56 +0100

    Fin de projet

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,28 +1,59 @@
 import React, { Component } from "react";
 
+const INTERVAL_BEFORE_MACHIN = 1000;
+
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { searchText: "", placeHolder: "Tapez un film..." };
+    this.state = {
+      searchText: "",
+      placeHolder: "Tapez un film...",
+      lockRequest: false,
+    };
   }
   render() {
     return (
       <div className="row">
-        <div className="col-md-8 my-5">
+        <div className="col-md-8 my-5 input-group">
           <input
             className="form-control input-lg"
             type="text"
             onChange={this.handleChange.bind(this)}
             placeholder={this.state.placeHolder}
           />
+          <span className="input-group-btn">
+            <button
+              className="btn btn-secondary"
+              onClick={this.handleOnClick.bind(this)}
+            >
+              Click here
+            </button>
+          </span>
         </div>
       </div>
     );
   }
 
   handleChange(event) {
-    console.log(event.target.value);
     this.setState({ searchText: event.target.value });
+    if (!this.state.lockRequest) {
+      this.setState({ lockRequest: true });
+      setTimeout(
+        function () {
+          this.search();
+        }.bind(this),
+        INTERVAL_BEFORE_MACHIN
+      );
+    }
+  }
+
+  handleOnClick(event) {
+    this.search();
+  }
+
+  search() {
+    this.props.callback(this.state.searchText);
+    this.setState({ lockRequest: false });
   }
 }
 
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
 import { SearchBar } from "../components/search-bar";
 import { VideoList } from "./video-list";
 import axios from "axios";
 import { VideoDetail } from "../components/video-detail";
 import { Video } from "../components/video";
+import { isEmpty, isNull } from "lodash";
 
 const API_END_POINT = "https://api.themoviedb.org/3/";
 const POPULAR_MOVIES_URL_START = "discover/movie?";
@@ -12,6 +12,8 @@ const API_KEY = "api_key=c202133b85d48a2c7fab900e9587716d";
 const POPULAR_MOVIES_URL_END =
   "&language=en-US&sort_by=popularity.desc&include_adult=false&append_to_response=images";
 const VIDEO_MOVIE_URL_END = "&language=en-US&include_adult=false";
+const SEARCH_URL = "search/movie?";
+const SEARCH_URL_END = "&language=en-US&include_adult=false";
 
 class App extends Component {
   constructor(props) {
@@ -30,6 +32,7 @@ class App extends Component {
       )
       .then(
         function (response) {
+          console.log(this.state);
           this.setState(
             {
               movieList: response.data.results.slice(1, 6),
@@ -51,35 +54,73 @@ class App extends Component {
       .then(
         function (response) {
           // console.log("ICI", response);
-          const youtubeKey = response.data.results[0].key;
-          let newCurrentMovieState = this.state.currentMovie;
-          newCurrentMovieState.videoId = youtubeKey;
-          this.setState({ currentMovie: newCurrentMovieState });
-          console.log("ICI maintenant", this.state.currentMovie);
+          if (response.data.results.length > 0) {
+            const youtubeKey = response.data.results[0].key;
+            let newCurrentMovieState = this.state.currentMovie;
+            newCurrentMovieState.videoId = youtubeKey;
+            this.setState({ currentMovie: newCurrentMovieState });
+            console.log("ICI maintenant", this.state.currentMovie);
+          }
         }.bind(this)
       );
   }
 
-  receiveCallback(movie) {
+  onClickListItem(movie) {
     this.setState({ currentMovie: movie }, function () {
       this.applyVideoToCurrentMovie();
+      this.setRecommandation();
     });
   }
 
+  setRecommandation() {
+    axios
+      .get(
+        `${API_END_POINT}movie/${this.state.currentMovie.id}/recommendations?${API_KEY}`
+      )
+      .then(
+        function (response) {
+          this.setState({ movieList: response.data.results.slice(0, 5) });
+        }.bind(this)
+      );
+  }
+
+  onClickSearch(searchText) {
+    if (searchText) {
+      axios
+        .get(
+          `${API_END_POINT}${SEARCH_URL}${API_KEY}&query=${searchText}${SEARCH_URL_END}`
+        )
+        .then(
+          function (response) {
+            if (response.data && response.data.results.length > 0) {
+              if (response.data.results[0].id != this.state.currentMovie.id) {
+                this.setState(
+                  { currentMovie: response.data.results[0] },
+                  () => {
+                    this.applyVideoToCurrentMovie();
+                    this.setRecommandation();
+                  }
+                );
+              }
+            }
+          }.bind(this)
+        );
+    }
+  }
   render() {
     const renderVideoList = () => {
       if (this.state.movieList.length >= 5) {
         return (
           <VideoList
             movieList={this.state.movieList}
-            callback={this.receiveCallback.bind(this)}
+            callback={this.onClickListItem.bind(this)}
           />
         );
       }
     };
     return (
       <React.Fragment>
-        <SearchBar />
+        <SearchBar callback={this.onClickSearch.bind(this)} />
         <div className="row">
           <div className="col-md-8">
             <Video videoId={this.state.currentMovie.videoId} />
